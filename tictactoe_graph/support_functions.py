7ed1997827c3d0986807a0e4c156595eda4b467b
Paul Hiemstra <p.h.hiemstra@windesheim.nl>
2021-03-01 15:31:28 +0100

    Created YT video for qtable

--- tictactoe_graph/support_functions.py
+++ tictactoe_graph/support_functions.py
@@ -6,6 +6,7 @@ import copy
 import hashlib
 import dill
 import itertools
+from plotnine import *
 
 class Tictoe:
     def __init__(self, size):
@@ -21,7 +22,7 @@ class Tictoe:
         self.possible_next_moves = copy.deepcopy(self.letters_to_move)
         self.moves_made = ''
     def get_board(self):
-        return self.board.reshape([self.size, self.size])
+        return np.copy(self.board.reshape([self.size, self.size]))
     def make_move(self, who, where, verbose=False):
         self.board[self.letters_to_move.index(where)] = who
         self.moves_made += where
@@ -116,6 +117,7 @@ def determine_move(tree, current_id, is_max):
 class Player_vs_itself:
     def __init__(self, id, alpha = 0.5, gamma = 0.6, epsilon = 0.1):
         self.qtable = {}
+        self.board_to_state = {}
         self.id = id
         self.alpha = alpha
         self.gamma = gamma
@@ -171,9 +173,31 @@ class Player_vs_itself:
         
         return game
 
+class memory_dict(dict):
+    def __init__(self, *args):
+        dict.__init__(self, args)
+        self.memory = {}
+
+    def __getitem__(self, key):
+        val = dict.__getitem__(self, key)
+        return val
+
+    def __setitem__(self, key, val):
+        if key in self.keys():
+            try:
+                self.memory[key].append(val)
+            except KeyError:
+                self.memory[key] = [self[key], val]
+        dict.__setitem__(self, key, val)
+    
+    def get_memory(self, key):
+        return self.memory[key]
+    
 class Player_vs_tree:
-    def __init__(self, id, alpha = 0.5, gamma = 0.6, epsilon = 0.1):
+    def __init__(self, id, tree, alpha = 0.5, gamma = 0.6, epsilon = 0.1):
         self.qtable = {}
+        self.board_to_state = {}
+        self.state_list = []
         self.id = id
         self.alpha = alpha
         self.gamma = gamma
@@ -182,6 +206,8 @@ class Player_vs_tree:
         self.tree = tree
     def get_qtable(self):
         return self.qtable
+    def get_board_to_state(self):
+        return self.board_to_state
     def get_id(self):
         return self.id
     def set_params(self, 
@@ -191,53 +217,83 @@ class Player_vs_tree:
         self.alpha = alpha
         self.gamma = gamma
         self.epsilon = epsilon
-    def make_move(self, game, verbose=False):
+    def make_move(self, game):
         if game.is_endstate():
             # If the game is done by the time we get to make a move, simply skip this step
             return game
         # Make a choice what move to take next
         possible_moves = game.get_possible_next_moves()
-        current_state = game.get_current_state()
+        self.current_state = game.get_current_state()
         
         # If the current_state does not exist in the qtable, insert it
-        if current_state not in self.qtable:
+        if self.current_state not in self.qtable:
             # New entry in the qtable, init to zero. 
-            #self.qtable[current_state] = dict(zip([current_state + move for move in possible_moves], 
-            #                                      np.random.uniform(0, 0.1, len(possible_moves))))  # initialize on a small amount of random noise. Promotes varyiety
-            self.qtable[current_state] = dict(zip(possible_moves, 
-                                                  np.zeros(len(possible_moves))))  
+            self.board_to_state[self.current_state] = game.get_board()
+            #action_vs_qvalue = memory_dict()    # We use a memory dict here so that we store the intermediate values
+            action_vs_qvalue = dict()
+            for action in possible_moves:
+                action_vs_qvalue[action] = 0
+            self.state_list.append(self.current_state)  # For plotting later
+            self.qtable[self.current_state] = action_vs_qvalue
             
         # Insert epsilon choice here, exploit or explore
         if random.uniform(0, 1) < self.epsilon:
-            new_state, reward, action = game.make_move(self.id, random.choice(possible_moves))   # Random choice
+            self.new_state, self.reward, self.action = game.make_move(self.id, random.choice(possible_moves))   # Random choice
         else:  # Exploit our qtable
-            new_state, reward, action = game.make_move(self.id, keywithmaxval(self.qtable[current_state]))   # Optimal choice
+            self.new_state, self.reward, self.action = game.make_move(self.id, keywithmaxval(self.qtable[self.current_state]))   # Optimal choice
         
-        if self.epsilon == 0:    # If we set epsilon to 0, we only want to play. No updating needed. 
-            return game
-        
+        return game  
+    def make_computer_move(self, game):
         # When we play against the tree, we make the tree give us the next move it makes
         # so in essence we treat the tree as a black box that also changes the worldstate. 
         # And we also only learn from our own moves, and no longer access the alternative
         # qtable. 
-        new_state_after_tree, reward_after_tree, action_after_tree = game.make_move(-1, determine_move(self.tree, game.get_moves_made(), False))
-        
+        if self.id == 1:
+            self.new_state_after_tree, self.reward_after_tree, self.action_after_tree = game.make_move(-1, determine_move(self.tree, game.get_moves_made(), False))
+        if self.id == -1:
+            self.new_state_after_tree, self.reward_after_tree, self.action_after_tree = game.make_move(1, determine_move(self.tree, game.get_moves_made(), True))
+        return game
+    def update_qtable(self):
         # Update the qtable
-        old_value = self.qtable[current_state][action]
+        old_value = self.qtable[self.current_state][self.action]
         try:
-            next_max = max(self.qtable[new_state_after_tree].values())
+            next_max = max(self.qtable[self.new_state_after_tree].values())
         except KeyError:  # In case the tree for next state has not been made yet, simply return 0
             next_max = 0
         # Note that the reward we actually get is the reward after the tree has made its move. We then reverse that reward vs the lut to get our own. 
-        new_value = (1 - self.alpha) * old_value + self.alpha * (self.our_reward_lut[reward_after_tree] + self.gamma * next_max)
-        if verbose:
-            print('old: ', old_value, 'new_value: ', new_value, 'alpha: ', self.alpha, 'reward: ', self.our_reward_lut[reward_after_tree], 'gamma: ', self.gamma, 'next_max: ', next_max)
-        self.qtable[current_state][action] = new_value
+        new_value = (1 - self.alpha) * old_value + self.alpha * (self.our_reward_lut[self.reward_after_tree] + self.gamma * next_max)
+        self.qtable[self.current_state][self.action] = new_value
+    def plot_qtable(self):
+        def get_for_sha1(sha1_state):
+            def try_na(qt, action):
+                # See if an action exists for a given state, return NA if not
+                try:
+                    return qt[action]
+                except KeyError:
+                    return np.nan
 
-        if verbose:
-            print(self.id, current_state, self.qtable[current_state], new_state_after_tree, action, old_value, new_value)
+            text_lut = {0: np.nan, 1: 'X', -1: 'O'}
+            return pd.DataFrame({'x': np.tile([1, 2, 3], 3), 'y': np.repeat([1, 2, 3], 3), 
+                          'board_state': [text_lut[val] for val in self.board_to_state[sha1_state].flatten()], 
+                          'q_values': [try_na(self.qtable[sha1_state], action) for action in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']], 
+                          'sha1_state': sha1_state})
+        plot_data = pd.concat([get_for_sha1(sha1) for sha1 in self.board_to_state.keys()])
         
-        return game    
+        # Sort states from the beginning to later in the game
+        #plot_data['board_na'] = plot_data['board_state'].isna()
+        #board_state_sorted = plot_data.groupby(['sha1_state'])['board_na'].sum().sort_values(ascending=False).index
+        plot_data['sha1_state'] = pd.Categorical(plot_data['sha1_state'], categories=self.state_list)
+        
+        return (
+            ggplot(plot_data, aes(x = 'x', y = 'y')) + 
+                geom_tile(aes(fill = 'q_values')) + 
+                geom_text(aes(label = 'board_state'), color = 'white') + 
+                scale_fill_gradient2() + 
+                scale_y_reverse() + 
+                facet_wrap('~ sha1_state') + 
+                theme(figure_size=(22,22), axis_text=element_blank(), axis_ticks=element_blank(), 
+                      strip_text_x = element_blank(), axis_title=element_blank())
+        )
 
 class Player_tree:
     def __init__(self, tree, id):
